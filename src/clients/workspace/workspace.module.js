d27cb0b5d2c77b48053a1cd503cf38a848cc50dd
Panos <pkokki@gmail.com>
2015-05-14 18:38:00 +0300

    workspace initial commit

---
+++ src/clients/workspace/workspace.module.js
@@ -0,0 +1,64 @@
+angular.module('workspace', [
+		'ui.router',
+		'ngMaterial',
+		'ngMessages',
+		'atlas',
+		'titan',
+    ])
+    .config(['$stateProvider', '$urlRouterProvider', '$mdIconProvider', function ($stateProvider, $urlRouterProvider, $mdIconProvider) {
+
+        $mdIconProvider.iconSet('core', '../assets/img/core-icons.svg', 24);
+
+		$urlRouterProvider.otherwise('/');
+
+        $stateProvider
+			.state('home', {
+				url: '/',
+				templateUrl: 'home.html',
+				controller: 'homeController',
+			})
+            .state('inbox', {
+				url: '/inbox',
+				templateUrl: 'inbox.html',
+				controller: 'inboxController',
+			})
+            ;
+    }])
+
+    /************************************************************************
+	 ****************************** SERVICES ********************************
+	 ************************************************************************/
+    .factory('spa', [function() {
+        return {
+            isSignedOn: function() { return true; }
+        };
+    }])
+
+    /************************************************************************
+ 	 **************************** DIRECTIVES ********************************
+ 	 ************************************************************************/
+
+    /************************************************************************
+  	 **************************** CONTROLLERS *******************************
+  	 ************************************************************************/
+  	.controller('appController', ['$scope', '$mdSidenav', '$state', 'spa', function($scope, $mdSidenav, $state, spa) {
+        $scope.goHome = function() {
+        	$state.go('home');
+        };
+        $scope.hasUser = spa.isSignedOn;
+
+        $scope.goUserProfile = function() {
+        	$state.go('userProfile');
+        };
+        $scope.toggleMenu = function(id) {
+        	$mdSidenav(id).toggle();
+        };
+        console.info('Workspace started');
+    }])
+  	.controller('homeController', ['$state', function($state) {
+          $state.go('inbox');
+    }])
+    .controller('inboxController', ['$scope', function($scope) {
+
+    }])
+    ;
