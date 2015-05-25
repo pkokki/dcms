8f7d46b7ef02578d5175b495fa64579d3de8a138
Panos <pkokki@gmail.com>
2015-05-25 10:36:58 +0300

    created atlas.sdk.module

---
+++ src/clients/common/atlas.sdk.module.spec.js
@@ -0,0 +1,7 @@
+describe('atlas.sdk', function() {
+    //beforeEach(module('atlas.sdk'));
+
+    it("contains spec with an expectation", function() {
+        expect(true).toBe(true);
+    });
+});
